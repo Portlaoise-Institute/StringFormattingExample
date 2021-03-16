bc47efd3a8a174fc5d4768d55064f3d7861e0d3e
dazwfd14 <dstones@portlaoisecollege.ie>
2021-03-16 09:20:34 +0000

    Upload to Git

--- src/string/formatting/example/StringFormattingExample.java
+++ src/string/formatting/example/StringFormattingExample.java
@@ -8,13 +8,15 @@ public class StringFormattingExample {
         String heading1 = "Exam_Name";
         String heading2 = "Exam_Grade";
         
+        //Table Data
         String exam1 = "Java";
         String grade1 = "A";
-        String exam2 = "PHP";
+        String exam2 = "Phyton";
         String grade2 = "B";
         String exam3 = "VB NET";
         String grade3 = "A";
         
+        //Output info to console window
         
         System.out.printf("%-15s %15s %n",heading1, heading2);
         System.out.println("---------------------------------"
@@ -22,6 +24,5 @@ public class StringFormattingExample {
         System.out.printf("%-15s %10s %n",exam1, grade1);
         System.out.printf("%-15s %10s %n",exam2, grade2);
         System.out.printf("%-15s %10s %n",exam3, grade3);
-    } //end main 
-    
+    } //end main    
 } //end class
